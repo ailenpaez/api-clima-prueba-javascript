b4ee31649f81085eb3d40603c7a330ff17abf0f9
ailenpaez <a.jorgelinapaez@gmail.com>
2023-12-11 00:12:58 -0300

    creación de segunda función del model

--- src/model/model.js
+++ src/model/model.js
@@ -5,24 +5,48 @@ const PATH = "../database/db.json";
 const getDataWeather = () => {
   const bufferData = readFileSync(PATH);
   const parsedData = JSON.parse(bufferData);
-//   console.log(parsedData); // prueba data
+  //   console.log(parsedData); // prueba data
   return parsedData;
 };
 
-function getHourlyTemperatureAndHumidity() {
-    const weatherData = getDataWeather();
-    // console.log(weatherData);
-    if (!weatherData || !weatherData.timelines || !weatherData.timelines.hourly) {
-        return false
-    }
+// getHourlyTemperatureAndHumidity() =>
+// DEBE RETORNAR UNA COLLECTION CON LOS SIGUIENTES OBJETOS:
+// {time: '2023-07-21T14:00:00Z', humidity: 86, temperature: 13.26}.
 
-    return weatherData.timelines.hourly.map(timeline => ({
-        time: timeline.time,
-        humidity: timeline.values.humidity,
-        temperature: timeline.values.temperature
-    }));
-}
+const getHourlyTemperatureAndHumidity = () => {
+  const weatherData = getDataWeather();
+  // console.log(weatherData);
+  if (!weatherData || !weatherData.timelines || !weatherData.timelines.hourly) {
+    return false;
+  }
+
+  return weatherData.timelines.hourly.map((timeline) => ({
+    time: timeline.time,
+    humidity: timeline.values.humidity,
+    temperature: timeline.values.temperature,
+  }));
+};
 
 const probarFuncion = getHourlyTemperatureAndHumidity();
 console.table(probarFuncion);
 
+// getPressureLevelByTemperature(temperature) =>
+// DEBE RETORNAR UNA COLLECTION QUE EN LA PROP 'temperature' sea >= AL VALOR 'temperature' RECIBIDO X PARÁMETRO.
+// FORMATO DE LA COLLECTION:
+// {pressureSurfaceLeve: 1009.62, temperature: 21.25}.
+
+const getPressureLevelByTemperature = (temperature) => {
+  const weatherData = getDataWeather();
+  if (!weatherData || !weatherData.timelines || !weatherData.timelines.hourly) {
+    return false;
+  }
+
+  return weatherData.timelines.hourly.filter((timeline) => timeline.values.temperature >= temperature).map((timeline) => ({
+      pressureSurfaceLeve: timeline.values.pressureSurfaceLevel,
+      temperature: timeline.values.temperature,
+    }));
+};
+
+const tempTest = 8;
+const probarPressureLevels = getPressureLevelByTemperature(tempTest);
+console.table(probarPressureLevels);
